109d0d7a8018352694a4ca68e3c07cacd06f913b
Santiago Zuluaga <santiagozh1998@gmail.com>
2020-11-22 00:09:01 -0500

    Manejando posibles errores y mostrando alertas en consola.

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import os
 from PIL import Image
 
-#Loading all files (images) in folder ImageToResize
-files = os.listdir('./ImagesToResize')
+if os.path.exists('ImagesToResize'):
+    #Loading all files (images) in folder ImageToResize
+    files = os.listdir('./ImagesToResize')
 
-for file in files:
-    #Each file (image) is opening and loading in Img
-    Img = Image.open('./ImagesToResize/'+file)
-    #Createing a new size
-    newSize = [Img.size[0]/2, Img.size[1]/2]
-    #Image size is change for newSize
-    Img.thumbnail(newSize)
-    #Image with new size is save in folder Outputs
-    Img.save('./Outputs/'+file)
+    if not os.path.exists('Outputs'):
+        os.makedirs('Outputs')
+
+    if len(files) != 0:
+
+        for file in files:
+            #Each file (image) is opening and loading in Img
+            Img = Image.open('./ImagesToResize/'+file)
+            #Createing a new size
+            newSize = [Img.size[0]/2, Img.size[1]/2]
+            #Image size is change for newSize
+            Img.thumbnail(newSize)
+            #Image with new size is save in folder Outputs
+            Img.save('./Outputs/'+file)
+    else:
+        print("There are no images in the folder")
+
+else:
+    print("Folder ImagesToResize don't exists")
