180e611e7f79c17f1639eb6581468ff4f54a66f2
Santiago Zuluaga <santiagozh1998@gmail.com>
2020-11-22 18:08:13 -0500

    Ingresando opcion de manejo de escalamiento de la imagen, manejo de errores en el input.

--- main.py
+++ main.py
@@ -11,15 +11,26 @@ if os.path.exists('ImagesToResize'):
     if len(files) != 0:
 
         for file in files:
+
+            scale = 0
             print(file)
+            while True:
+                inputscale = input("Enter a scale to resize image: ")
+                try:
+                    scale = float(inputscale)
+                    break
+                except ValueError:
+                    print("Scale is not valid.")
+            
             #Each file (image) is opening and loading in Img
             Img = Image.open('./ImagesToResize/'+file)
             #Createing a new size
-            newSize = [Img.size[0]/2, Img.size[1]/2]
+            newSize = [Img.size[0]/scale, Img.size[1]/scale]
             #Image size is change for newSize
             Img.thumbnail(newSize)
             #Image with new size is save in folder Outputs
             Img.save('./Outputs/'+file)
+            print("Reized image succesfully.")
     else:
         print("There are no images in the folder")
 
